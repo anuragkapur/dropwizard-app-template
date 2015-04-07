6fa816ae037c2526ff0682f3de88f0febd1732b8
Anurag Kapur <anuragkapur@gmail.com>
2015-04-07 14:52:10 +0100

    Added tempalte app

---
+++ app/src/main/java/com/ft/membership/appName/App.java
@@ -0,0 +1,56 @@
+package com.ft.membership.appName;
+
+import com.ft.membership.appName.health.HelloWorldHealthCheck;
+import com.ft.membership.appName.resources.HelloWorldResource;
+import com.google.common.io.Resources;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+/**
+ * @author anuragkapur
+ */
+public class App extends Application<AppConfig> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    @Override
+    public void run(AppConfig appConfig, Environment environment) throws Exception {
+
+        // Init and register hello-world resource
+        final HelloWorldResource helloWorldResource = new HelloWorldResource(appConfig.getGreeting());
+        environment.jersey().register(helloWorldResource);
+
+        final HelloWorldHealthCheck helloWorldHealthCheck = new HelloWorldHealthCheck();
+        environment.healthChecks().register("helloWorldHealthCheck", helloWorldHealthCheck);
+    }
+
+    @Override
+    public String getName() {
+        return "login-service";
+    }
+
+    public static void main(String[] args) throws Exception {
+        new App().run(getDefaultedArgs(args));
+    }
+
+    private static String[] getDefaultedArgs(String[] args) {
+        final String[] actualArgs;
+        if (args.length == 0) {
+            String yamlPath = null;
+            try {
+                yamlPath = new File(Resources.getResource("com/ft/membership/appName/local-dev.yaml").toURI()).getAbsolutePath();
+            } catch (URISyntaxException e) {
+                LOGGER.error("error reading config", e);
+            }
+            actualArgs = new String[]{"server", yamlPath};
+        } else {
+            actualArgs = args;
+        }
+        return actualArgs;
+    }
+}
